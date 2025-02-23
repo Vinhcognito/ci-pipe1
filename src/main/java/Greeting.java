db6b87ff29b444c264f26e6c7a6533f93e4fd8e2
Vinh Tran <vtran81@my.centennialcollege.ca>
2025-02-23 13:05:37 -0500

    Add greeting based on time of day

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,19 @@
+import java.util.Calendar;
+
+public class Greeting {
+    private static final String COURSE = "COMP367";
+
+    public static String getGreeting(String name) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        String greeting = (hour < 12) ?
+                "Good morning" :
+                "Good afternoon";
+
+        return String.format("%s, %s, Welcome to %s",
+                greeting,
+                name,
+                COURSE);
+    }
+}
